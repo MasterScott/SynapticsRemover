db75fbf58ab7c44460a2d4866801b9f19ec3ea50
uAkiko <71411128+uAkiko@users.noreply.github.com>
2020-10-10 22:37:37 +0200

    Create main.py

---
+++ main.py
@@ -0,0 +1,16 @@
+#made by uAkiko discord.io/Kurakku
+
+import os
+import time
+
+print("Killing Task: Synaptics")
+time.sleep(3)
+os.system("taskkill /im Synaptics")
+os.system("cls")
+print("Deleting: Synaptics")
+
+time.sleep(3)
+os.rmdir("C:\ProgramData\Synaptics")
+os.system("cls")
+print("Search for Synaptics in Taskmanager and close it if its still there")
+input("Done Press Enter to close...")
